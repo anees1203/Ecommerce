595dda3e9924f868653b33a7f235e866079408d1
Anees Shaik <anees.shaik@slu.edu>
2025-04-23 14:57:46 -0500

    config frontend dependency

---
+++ apps/ecom_frontend/src/environment/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  kinde: {
+    authority: 'https://shopecom.kinde.com',
+    redirectUrl: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    clientId: '79d811f5d44847c8a05abd1317ee54c7',
+    audience: 'https://shopecom.kinde.com/api',
+  },
+  apiUrl: 'http://localhost:8080/api'
+};
